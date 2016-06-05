2052a8f4bc28ba3f0f92f7fc2d19fe893a1a97bd
Li Lin <guru.lin@gmail.com>
2016-06-05 14:17:51 +0800

    weaterh list loads without charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,12 @@ import axios from 'axios';
 const API_KEY = 'af138576caaf34033beaff441d1f641b';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const FETCH_WATHER = 'FETCH_WATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  console.log(url);
-
   return {
-    type: FETCH_WATHER,
+    type: FETCH_WEATHER,
     payload: axios.get(url)
   };
 }
